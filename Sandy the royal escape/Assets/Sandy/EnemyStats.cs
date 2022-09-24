5f75053698892d69eacfe360ff61f64bedae0e6f
Meerkatgirl118 <114282076+Meerkatgirl118@users.noreply.github.com>
2022-09-24 09:11:47 +0100

    The project so far.

---
+++ Sandy the royal escape/Assets/Sandy/EnemyStats.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    // Level 1
+
+    public int cleaningRatAttack;
+    public int cleaningRatHealth;
+
+    public int cookingRatAttack;
+    public int cookingRatHealth;
+
+    public int armyRatAttack;
+    public int armyRatHealth;
+
+    public int securityRatAttack;
+    public int securityRatHealth;
+
+    public int securityBlindMiceAttack;
+    public int securityBlindMiceHealth;
+
+    // Level 2
+
+    public int royalDeadAdvisorsAttack;
+    public int royalDeadAdvisorsHealth;
+
+    public int royalDeadChefAttack;
+    public int royalDeadChefHealth;
+
+    public int executionerAttack;
+    public int executionerHealth;
+
+    // Level 3
+
+    public int knightRatAttack;
+    public int knightRatHealth;
+
+    public int queenJuliaAttack;
+    public int queenJuliaHealth;
+}
